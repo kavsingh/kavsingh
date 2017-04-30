317a76e47abe79ac34dd53b21fc6fa80188fdf16
Kav Singh <flakisms@gmail.com>
2017-04-30 20:00:54 +1000

    simple https for local dev.

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 /* eslint-env node */
+/* eslint-disable no-console, global-require */
 
 const express = require('express')
 const path = require('path')
 const next = require('next')
+const fs = require('fs')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dir: '.', dev })
 const handle = app.getRequestHandler()
+const tilde = path.resolve.bind(path, process.env.HOME)
 
 app.prepare().then(() => {
   /*
     Incorporate service worker as per https://github.com/ooade/NextSimpleStarter
   */
-  const server = express()
+  const expressApp = express()
 
   // serve service worker
-  server.get('/sw.js',
+  expressApp.get('/sw.js',
     (req, res) => res.sendFile(path.resolve('./.next/sw.js')))
 
-  server.get('*', (req, res) => handle(req, res))
+  expressApp.get('*', (req, res) => handle(req, res))
+
+  // SSL for local dev to test service worker
+  const server = dev
+    ? require('https').createServer({
+      cert: fs.readFileSync(tilde('.localhost-ssl/cert.pem')),
+      key: fs.readFileSync(tilde('.localhost-ssl/key.pem')),
+    }, expressApp)
+    : expressApp
 
   server.listen(3000, err => {
     if (err) throw err
