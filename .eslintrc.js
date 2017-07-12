8121bff974a560d3f4c3a2a4ff7b668f64739147
Kav Singh <flakisms@gmail.com>
2017-07-12 17:17:10 +1000

    update linter config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,10 @@ module.exports = {
     browser: true,
     node: false,
   },
-  extends: 'eslint-config-mongrel-react',
-  rules: {
-    'max-len': ['warn', {
-      code: 80,
-      tabWidth: 2,
-      ignoreComments: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-  }
+
+  settings: {
+    'import/resolver': 'webpack',
+  },
+
+  extends: ['mongrel-react'],
 }
--- components/Article/index.js
+++ components/Article/index.js
@@ -3,39 +3,37 @@ import { screenTheme, printTheme } from '../../style/color'
 import { callIfFn } from '../../util/function'
 import { renderable } from '../../propTypes'
 
-const Article = ({ title, meta, body }) => {
-  return (
-    <article className="article">
-      {title ? <h1 className="article__title">{callIfFn(title)}</h1> : null}
-      {meta ? <h2 className="article__meta">{callIfFn(meta)}</h2> : null}
-      <div className="article__body">{callIfFn(body)}</div>
-      <style jsx>{`
-        .article {
-          page-break-inside: avoid;
-        }
+const Article = ({ title, meta, body }) => (
+  <article className="article">
+    {title ? <h1 className="article__title">{callIfFn(title)}</h1> : null}
+    {meta ? <h2 className="article__meta">{callIfFn(meta)}</h2> : null}
+    <div className="article__body">{callIfFn(body)}</div>
+    <style jsx>{`
+      .article {
+        page-break-inside: avoid;
+      }
 
-        .article__title {
-          font-size: 1.1em;
-          margin: 0 0 0.2em;
-        }
+      .article__title {
+        font-size: 1.1em;
+        margin: 0 0 0.2em;
+      }
 
+      .article__meta {
+        font-size: 0.88em;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin: 0 0 1.1em;
+        color: ${screenTheme.bodyTextSecondary};
+      }
+
+      @media print {
         .article__meta {
-          font-size: 0.88em;
-          font-weight: 400;
-          text-transform: uppercase;
-          margin: 0 0 1.1em;
-          color: ${screenTheme.bodyTextSecondary};
+          color: ${printTheme.bodyTextSecondary};
         }
-
-        @media print {
-          .article__meta {
-            color: ${printTheme.bodyTextSecondary};
-          }
-        }
-      `}</style>
-    </article>
-  )
-}
+      }
+    `}</style>
+  </article>
+)
 
 Article.propTypes = {
   title: renderable,
