4cbe28e6f935f1d232135c81e650df84215bd413
Kav Singh <flakisms@gmail.com>
2023-08-13 03:15:24 +0200

    use tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
-/** @type {import("tailwindcss").Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			screens: {
+				screen: { raw: "screen" },
 				print: { raw: "print" },
 			},
 		},
 	},
-};
+} satisfies Config;
