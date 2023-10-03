7f1ec9089252a0b175d0edc9e34733d0fb1ce9fa
Kav Singh <flakisms@gmail.com>
2023-10-03 21:09:30 +0200

    add vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
 	site: "https://kavsingh.com",
 	integrations: [tailwind()],
+	adapter: vercel({ webAnalytics: { enabled: true } }),
 });
