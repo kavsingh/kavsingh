ed36027ec7a5fc4826286d640770073f580c11fd
Kav Singh <flakisms@gmail.com>
2024-05-16 06:33:38 +0200

    update deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				print: "Avenir, Aptos, system-ui, sans-serif",
+			},
 			screens: {
 				screen: { raw: "screen" },
 				print: { raw: "print" },
