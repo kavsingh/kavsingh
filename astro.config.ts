d7a1c51398977df20371612bfafc5a667c85222a
Kav Singh <flakisms@gmail.com>
2024-07-27 19:14:00 +0200

    update deps

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 
+import type { AstroIntegration } from "astro";
+
 export default defineConfig({
 	site: "https://kavsingh.com",
-	integrations: [tailwind()],
+	integrations: [tailwind() as unknown as AstroIntegration],
 	adapter: vercel({ webAnalytics: { enabled: true } }),
 });
