b1a88c107350dad9bc9e30b8056ddb516bbea273
Kav Singh <flakisms@gmail.com>
2026-03-24 00:29:17 +0100

    fix: content loaders

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,6 @@
 import { defineCollection } from "astro:content";
+
+import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const baseSchema = z.object({
@@ -9,15 +11,12 @@ const baseSchema = z.object({
 });
 
 const experienceCollection = defineCollection({
-	type: "content",
-	schema: baseSchema.extend({
-		employer: z.string(),
-		position: z.string(),
-	}),
+	loader: glob({ base: "./src/content/experience", pattern: "**/*.{md,mdx}" }),
+	schema: baseSchema.extend({ employer: z.string(), position: z.string() }),
 });
 
 const educationCollection = defineCollection({
-	type: "content",
+	loader: glob({ base: "./src/content/education", pattern: "**/*.{md,mdx}" }),
 	schema: baseSchema.extend({
 		institution: z.string(),
 		program: z.string(),
